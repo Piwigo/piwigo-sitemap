1a4ad2bac6144ae19af8c42598182fe481264a99
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-28 07:24:33 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Filename'] = 'Skráarheiti';
+$lang['Frequency'] = 'Tíðni';
+$lang['Global'] = 'Almennt';
+$lang['Priority'] = 'Forgangur';
+$lang['always'] = 'alltaf';
+$lang['daily'] = 'daglega';
+$lang['hourly'] = 'á klukkutíma fresti';
+$lang['monthly'] = 'mánaðarlega';
+$lang['never'] = 'aldrei';
+$lang['weekly'] = 'vikulega';
+$lang['yearly'] = 'árlega';
