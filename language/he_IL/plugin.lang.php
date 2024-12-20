663df6a16202c47dd2a7183bba0f5bfb918583d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:03:36 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Filename'] = 'שם קובץ';
+$lang['Frequency'] = 'תדירות';
+$lang['Global'] = 'גלובלי';
+$lang['Priority'] = 'עדיפות';
+$lang['always'] = 'תמיד';
+$lang['daily'] = 'יומי';
+$lang['hourly'] = 'שעתי';
+$lang['monthly'] = 'חודשי';
+$lang['never'] = 'אף פעם';
+$lang['weekly'] = 'שבועי';
+$lang['yearly'] = 'שנתי';
