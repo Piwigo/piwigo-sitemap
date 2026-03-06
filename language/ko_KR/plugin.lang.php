c6c06694d9e9ac818de2f7c58902ea0f5aa1fb3c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-06 17:44:40 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Filename'] = '파일명';
+$lang['Frequency'] = '빈도';
+$lang['Global'] = '전역';
+$lang['Priority'] = '우선순위';
+$lang['always'] = '항상';
+$lang['daily'] = '매일';
+$lang['hourly'] = '매시간';
+$lang['monthly'] = '매월';
+$lang['never'] = '절대 안 함';
+$lang['weekly'] = '매주';
+$lang['yearly'] = '매년';
